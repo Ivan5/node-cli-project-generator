8b94caaa2184aea34ea0c67cf1187a543645b0bd
Ivan5 <converse_ivan@hotmail.com>
2020-11-03 16:14:16 -0600

    copy files in the new directory

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ inquirer.prompt(QUESTIONS).then((respuestas) => {
   const pathTarget = path.join(DIR_ACTUAL, proyecto);
 
   createProject(pathTarget);
+  createDirectoriesFilesContent(templatePath, proyecto);
 });
 
 function createProject(projectPath) {
@@ -49,4 +50,27 @@ function createProject(projectPath) {
   }
   fs.mkdirSync(projectPath);
   return true;
+}
+
+function createDirectoriesFilesContent(templatePath, proyecto) {
+  const listFileDirectories = fs.readdirSync(templatePath);
+
+  listFileDirectories.forEach((item) => {
+    const originalPath = path.join(templatePath, item);
+
+    const stats = fs.statSync(originalPath);
+
+    const writePath = path.join(DIR_ACTUAL, proyecto, item);
+
+    if (stats.isFile()) {
+      let content = fs.readFileSync(originalPath, "utf-8");
+      fs.writeFileSync(writePath, content, "utf-8");
+    } else if (stats.isDirectory()) {
+      fs.mkdirSync(writePath);
+      createDirectoriesFilesContent(
+        path.join(templatePath, item),
+        path.join(proyecto, item)
+      );
+    }
+  });
 }
